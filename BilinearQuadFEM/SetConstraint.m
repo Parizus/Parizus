a266f06ad179ded989c4ecb18a1dc929e60268a5
Parizus <hellopach@live.com>
2016-04-09 14:13:41 +0800

    Revert "create"
    This reverts commit a958f8597c02b6392c50feee1a42fa86029a69fe.

--- BilinearQuadFEM/SetConstraint.m
+++ BilinearQuadFEM/SetConstraint.m
@@ -1,9 +1,8 @@
 function [Disp,connum]=SetConstraint(jdx,jdy)
     % 设置边界条件
     Disp(1:jdx*jdy,1:2)=1;
-%     constraints=1:jdx:jdx*jdy;
-%     constraints=[constraints,jdx:jdx:jdx*jdy];
-    constraints=[(jdy-1)/2*jdx+1,(jdy-1)/2*jdx+jdx];
+    constraints=1:jdx:jdx*jdy;
+    constraints=[constraints,jdx:jdx:jdx*jdy];
     connum_=size(constraints);
     connum=connum_(2);
     Disp(constraints,:)=0;
