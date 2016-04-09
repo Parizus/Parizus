502138532739a38aeb3e668ed2dc2a819a327152
Parizus <hellopach@live.com>
2016-04-09 15:30:05 +0800

    这里！！！！

--- BilinearQuadFEM/CalculateSN.m
+++ BilinearQuadFEM/CalculateSN.m
@@ -1,20 +1,20 @@
-function sn = CalculateSN(ek,em,en,Disp,ith,cf,Mode,Modenum,dispelnum)
+function sn = CalculateSN(ek,em,en,Disp_,ith,cf,Mode,Modenum,nodnum)
 	%system stiffness matrix
-	k(1:2*dispelnum,1:2*dispelnum)=0;
+	k(1:2*nodnum,1:2*nodnum)=0;
 	%system mass matrix
-	m(1:2*dispelnum,1:2*dispelnum)=0;
+	m(1:2*nodnum,1:2*nodnum)=0;
 	index(1:8)=0; % vector sontaining system dofs of nodes in each element.
 	loopi=ith;
 		for zi=1:4
-			index((zi-1)*2+1)=Disp(en(loopi,zi),1);
-			index((zi-1)*2+2)=Disp(en(loopi,zi),2);
+			index((zi-1)*2+1)=Disp_(en(loopi,zi),1);
+			index((zi-1)*2+2)=Disp_(en(loopi,zi),2);
 		end
 		for i=1:8
 			for j=1:8
-				if(index(i)*index(j)~=0)
+				% if(index(i)*index(j)~=0)
 					k(index(i),index(j))=ek(i,j);
 					m(index(i),index(j))=em(i,j);
-				end
+				% end
 			end
         end
 sn=Mode(:,Modenum)'*(k-cf(Modenum)^2/3*m)*Mode(:,Modenum)/2/cf(Modenum);
--- BilinearQuadFEM/main.m
+++ BilinearQuadFEM/main.m
@@ -2,20 +2,21 @@
 % 设置参数，杨氏模量(E)，泊松比(NU)，厚度(h)，密度(rho)
 E=210e6;NU=0.3;h=0.025;rho=1;
 % 设置参数，横向长度(lx)，纵向长度(ly)，横向节点数(jdx)，纵向节点数(jdy)
-lx=8;ly=1;jdx=33;jdy=5;
+lx=8;ly=1;jdx=33;jdy=5; 
 elnum=(jdx-1)*(jdy-1);
+nodnum=jdx*jdy;
 % 进行有限元计算
 % 输出各阶频率(cf)，模态矩阵(Mode)
 % 节点自由度-整体自由度编号矩阵(Disp)，单元结点编号-整体节点编号矩阵(en)
 % 单元刚度阵(ek)，单元质量阵(em)，自由度数(dispelnum)
-[cf,Mode,Disp,en,ek,em,dispelnum]=BilinearQuadSolve(E,NU,h,rho,lx,ly,jdx,jdy);
+[cf,Mode,Disp_,Disp,en,ek,em,dispelnum]=BilinearQuadSolve(E,NU,h,rho,lx,ly,jdx,jdy);
 % 显示模态结果
-% figure(1);
-% DisplayMode(jdx,jdy,Disp,Mode,1);
+figure(1);
+Mode_=DisplayMode(jdx,jdy,Disp,Mode,1);
 % 进行单元敏度计算
 sn=zeros(1,elnum);
 for i=1:elnum
-    sn(i)=CalculateSN(ek,em,en,Disp,i,cf,Mode,1,dispelnum);
+        sn(i)=CalculateSN(ek,em,en,Disp_,i,cf,Mode_,1,nodnum);
 end
 mapdata=reshape(sn,[jdx-1,jdy-1])';
 figure(2);
