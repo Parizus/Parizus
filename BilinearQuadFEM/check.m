fbd4f0a8058e626198b99adaa781d79678c3b1b1
Parizus <hellopach@live.com>
2016-04-17 22:04:16 +0800

    Revert "add great check"
    This reverts commit 69b87d514bd66d711b0a5bcb9b9afc22ba091929.

--- BilinearQuadFEM/check.m
+++ BilinearQuadFEM/check.m
@@ -1,35 +1,18 @@
-function sn=check(en,sn,rmin,lx,ly,jdx,jdy,nelx,nely,elnum)
-% 计算节点敏度
-    dof=zeros(jdx*jdy,2);
-    for i=1:elnum
-        for j=1:4
-            dof(en(i,j),1)=dof(en(i,j))+sn(i);
-            dof(en(i,j),2)=dof(en(i,j),2)+1;
-        end
+function [dcf]=check(jdx,jdy,rmin,sn)
+nelx=jdx-1;nely=jdy-1;
+mapdata=reshape(sn,[nelx,nely])';
+dcf=zeros(nely,nelx);
+for i = 1:nelx
+  for j = 1:nely
+    sum=0.0; 
+    for k = max(i-floor(rmin),1):min(i+floor(rmin),nelx)
+      for l = max(j-floor(rmin),1):min(j+floor(rmin),nely)
+       fac = rmin-sqrt((i-k)^2+(j-l)^2);
+       sum = sum+max(0,fac);
+       dcf(j,i) = dcf(j,i) + max(0,fac)*mapdata(l,k);
+      end
     end
-    dof_=reshape(dof(:,1)./dof(:,2),[jdx,jdy])';
-    % 计算单元尺寸，单元长度(el)，单元高度(eh)
-    el=lx/(jdx-1);
-    eh=ly/(jdy-1);
-    % 计算过滤后的单元敏度
-    rminl=floor(rmin/el);
-    rminh=floor(rmin/eh);
-    snt=zeros(nelx,nely);
-    count=snt;
-    for i=1:nelx
-        for j=1:nely
-            for ii=max(1,(i-rminl)):min(jdx,(i+rminl+1))
-                for jj=max(1,(j-rminh)):min(jdy,(j+rminh+1))
-                    dic=sqrt(((abs(i-ii+0.5))*el)^2+((abs(j-jj+0.5))*eh)^2);
-                    if dic<=rmin
-                        snt(i,j)=snt(i,j)+dof_(jj,ii)*(rmin-dic);
-                        count(i,j)=count(i,j)+(rmin-dic);
-                    end
-                end 
-            end
-        end
-    end
-    sn=snt./count;
-    a000=sn;
-    sn=reshape(sn,1,[]);
+    dcf(j,i) = dcf(j,i)/sum;
+  end
 end
+dcf=reshape(mapdata,1,[]);
--- BilinearQuadFEM/main.m
+++ BilinearQuadFEM/main.m
@@ -1,24 +1,22 @@
 % 设置参数，杨氏模量(E)，泊松比(NU)，厚度(h)，密度(rho)
-E=10e6;NU=0.3;h=1;rho=1;
+E=210e6;NU=0.3;h=0.025;rho=1;
 % 设置参数，横向长度(lx)，纵向长度(ly)，横向节点数(jdx)，纵向节点数(jdy)
+% lx=8;ly=1;jdx=321;jdy=41;
 lx=8;ly=1;jdx=321;jdy=41;
-% lx=8;ly=1;jdx=161;jdy=21;
 % 设置体积进化率(ER)，体积最大添加率(AR)，目标优化体积(Vf)
-ER=0.02;ARm=0.02;Vf=0.5;rmin=0.075;
+ER=0.05;ARm=0.02;Vf=0.5;rmin=0.3;
 % 计算单元总数
-nelx=jdx-1;nely=jdy-1;
-elnum=nelx*nely;
+elnum=(jdx-1)*(jdy-1);
 % 计算目标减少体积
 Vdel=0;
 Vaddm=round(ARm*elnum);
-Vfdel=round(Vf*elnum/4)*4;
+Vfdel=round(Vf*elnum);
 sn_(1,1:elnum)=1;
 sn=zeros(1,elnum);
 cyclenum=1;
-elnum_=elnum;
 TimeData(cyclenum,:)=clock;
-while(elnum_>=(nelx*nely-Vfdel))
-    Vdel=Vdel+round(ER*elnum_/4)*4;
+while(Vdel<=Vfdel)
+    Vdel=Vdel+round(ER*elnum);
     % 进行有限元计算
     % 输出各阶频率(cf)，模态矩阵(Mode)
     % 节点自由度-整体自由度编号矩阵(Disp)，单元结点编号-整体节点编号矩阵(en)
@@ -28,26 +26,21 @@ while(elnum_>=(nelx*nely-Vfdel))
     % figure(1);
     % DisplayMode(jdx,jdy,Disp,Mode,1);
     % 进行单元敏度计算
+    fprintf('%4.0f %d',clock,cyclenum);
+    fprintf('\n');
     for i=1:elnum
         sn(i)=CalculateSN(ek,em,en,Disp,i,cf,Mode,1,dispelnum,sn_(i));
     end
-    a001=sn;
-    a000=sn;
-    sn=check(en,sn,rmin,lx,ly,jdx,jdy,nelx,nely,elnum);    
-    if cyclenum>1
-        snold=(sn+snold)/2;
-    else
-        snold=sn;
-    end
+    sn=check(jdx,jdy,rmin,sn);
     % 删除低敏度单元
     [nd,sortindex]=sort(sn);
     sn_(sortindex(1:Vdel))=0;
     % 添加高敏度单元
-    countadd=elnum_-Vdel-sum(sum(sn_(sortindex(Vdel+1:elnum_))),2);
+    countadd=elnum-Vdel-sum(sum(sn_(sortindex(Vdel+1:elnum))),2);
     if (countadd<=Vaddm)
-        sn_(sortindex(Vdel+1:elnum_))=1;
+        sn_(sortindex(Vdel+1:elnum))=1;
     else
-        count=0;i=elnum_;
+        count=0;i=elnum;
         while (count<=Vaddm)
             if (sn_(sortindex(i))==0)
                 sn_(sortindex(i))=1;
@@ -57,16 +50,12 @@ while(elnum_>=(nelx*nely-Vfdel))
         end
         countadd=Vaddm;
     end
-    mapdata=reshape(sn_,[nelx,nely])';
+    mapdata=reshape(sn_,[jdx-1,jdy-1])';
     figure(2);
     % 绘制黑白图像
-    colormap(gray); imagesc(-mapdata); axis equal; axis tight; axis off;
-    elnum_=sum(sum(sn_),2);
-    % 输出控制
-    c=clock;
-    fprintf('%d %d %d %d %d %d %6.2f\n',c(1:5),cyclenum,cf(1));
+    colormap(gray); imagesc(-mapdata); axis equal; axis tight; axis off;pause(1e-6);
+    elnum=sum(sum(sn_),2);
     save(['example',num2str(cyclenum)]);
-    saveas(2,num2str(cyclenum),'jpg');
     cyclenum=cyclenum+1;
     TimeData(cyclenum,:)=clock;
 %     pause;
